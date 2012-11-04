bd4e60b840ce0133465cd00ee35f32d83aa4995b
Doc Ritezel <ritezel+github@gmail.com>
2012-11-04 14:42:07 -0800

    update readme
    Also add debugging output for solo.rb and node.json

--- lib/soloist.rb
+++ lib/soloist.rb
@@ -1,4 +1,2 @@
 require "soloist/version"
-require "soloist/royal_crown"
-require "soloist/config"
 require "soloist/cli"
--- lib/soloist/cli.rb
+++ lib/soloist/cli.rb
@@ -1,23 +1,38 @@
 require "librarian/chef/cli"
 require "soloist/spotlight"
 require "soloist/config"
+require "tempfile"
 require "thor"
 
 module Soloist
   class NotFound < RuntimeError; end
 
   class CLI < Thor
+    default_task :run_chef
+
     desc "run_chef", "Runs chef-solo like a baws"
     def run_chef
       ensure_chef_cache_path
       raise Soloist::NotFound.new("Could not find soloistrc") unless rc_path
-      File.open(solo_rb_path, "w+") { |f| f.write(config.as_solo_rb) }
-      File.open(node_json_path, "w+") { |f| f.write(config.as_json) }
+      write_solo_rb
+      write_node_json
       install_cookbooks
-      exec("sudo bash -c '#{environment} #{chef_solo_command}'")
+      exec("sudo bash -c '#{environment} #{chef_solo}'")
     end
 
     no_tasks do
+      def write_solo_rb
+        content = config.as_solo_rb
+        content.each{ |line| puts line } if log_level == "debug"
+        File.open(solo_rb.path, "w") { |f| f.write(content.join("\n")) }
+      end
+
+      def write_node_json
+        content = config.as_json
+        puts JSON.pretty_generate(content) if log_level == "debug"
+        File.open(node_json.path, "w") { |f| f.write(JSON.dump(content)) }
+      end
+
       def ensure_chef_cache_path
         unless File.directory?("/var/chef/cache")
           system("sudo mkdir -p /var/chef/cache")
@@ -34,8 +49,8 @@ module Soloist
     end
 
     private
-    def chef_solo_command
-      "chef-solo -j #{node_json_path} -c #{solo_rb_path} -l #{log_level}"
+    def chef_solo
+      "chef-solo -j '#{node_json.path}' -c '#{solo_rb.path}' -l '#{log_level}'"
     end
 
     def environment
@@ -46,12 +61,12 @@ module Soloist
       ENV["LOG_LEVEL"] || "info"
     end
 
-    def solo_rb_path
-      @solo_rb_path ||= Tempfile.new("solo.rb")
+    def solo_rb
+      @solo_rb ||= Tempfile.new(["solo", ".rb"])
     end
 
-    def node_json_path
-      @node_json_path ||= Tempfile.new("node.json")
+    def node_json
+      @node_json ||= Tempfile.new(["node", ".json"])
     end
 
     def config
