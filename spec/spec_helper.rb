6bdd03398f6fb15728721a53617d4e0b3bc5391e
Doc Ritezel and Matthew Kocher <pair+doc+mkocher@pivotallabs.com>
2012-11-30 19:21:23 -0800

    Fix specs assuming there is a soloistrc in the path

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,6 @@ Dir.glob(File.expand_path("../helpers/**/*.rb", __FILE__)) { |f| require f }
 
 RSpec.configure do |config|
   config.add_setting :tempdir
-  config.before(:each) {RSpec.configuration.tempdir = Dir.mktmpdir }
+  config.before(:each) { RSpec.configuration.tempdir = Dir.mktmpdir }
   config.after(:each) { FileUtils.rm_rf(RSpec.configuration.tempdir) }
 end
